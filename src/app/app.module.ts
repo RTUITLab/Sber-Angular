8be1c6aa60fe7b7ddf481733946d90a3b4d11163
IvLaptev <ivlaptev13@ya.ru>
2020-10-17 19:50:45 +0300

    Added page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LeftTabMenuComponent } from './left-tab-menu/left-tab-menu.component';
 import { ProgressComponent } from './progress/progress.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { AddModuleComponent } from './add-module/add-module.component';
+import { FirstStepComponent } from './first-step/first-step.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LeftTabMenuComponent,
-    ProgressComponent
+    ProgressComponent,
+    AddModuleComponent,
+    FirstStepComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
