11b26e212fb3a6fb2a00b3316e825576447eb62d
Mikhail <wowivan2011@yandex.ru>
2020-10-17 16:20:35 +0300

    init one router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { LeftTabMenuComponent } from './left-tab-menu/left-tab-menu.component';
+import { LeftTabMenuComponent } from './card/left-tab-menu/left-tab-menu.component';
+import { CardComponent } from './card/card.component';
+import { CardComponentComponent } from './card/card-component/card-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeftTabMenuComponent
+    LeftTabMenuComponent,
+    CardComponent,
+    CardComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: '', component: CardComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
