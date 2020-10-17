423ed4096e0ffef3de4b7823e494b5ed77106059
IvLaptev <ivlaptev13@ya.ru>
2020-10-17 18:33:53 +0300

    Added left progress component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LeftTabMenuComponent } from './left-tab-menu/left-tab-menu.component';
+import { ProgressComponent } from './progress/progress.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeftTabMenuComponent
+    LeftTabMenuComponent,
+    ProgressComponent
   ],
   imports: [
     BrowserModule,
