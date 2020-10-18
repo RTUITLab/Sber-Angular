a0b249c44b1c291ab3d33901f405813ba1bacc6d
Mikhail <wowivan2011@yandex.ru>
2020-10-18 04:57:54 +0300

    update api

--- src/api/models.ts
+++ src/api/models.ts
@@ -2,6 +2,7 @@ export { CommentStatus } from './models/comment-status';
 export { ModulePart } from './models/module-part';
 export { CommentResponse } from './models/comment-response';
 export { CreateCommentRequest } from './models/create-comment-request';
+export { ModuleStatus } from './models/module-status';
 export { ModuleCompactResponse } from './models/module-compact-response';
 export { ModuleVisibility } from './models/module-visibility';
 export { CreateEditModuleRequest } from './models/create-edit-module-request';
