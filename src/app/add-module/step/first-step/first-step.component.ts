fc510873424ededfe9ffe3116aa70ee85ff0569b
Mikhail <wowivan2011@yandex.ru>
2020-10-18 04:48:11 +0300

    Added the ability to change the first step

--- src/app/add-module/step/first-step/first-step.component.ts
+++ src/app/add-module/step/first-step/first-step.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { CreateEditModuleRequest, ModuleCompactResponse } from 'src/api/models';
+import { CreateEditModuleRequest, ModuleCompactResponse, ModuleResponse } from 'src/api/models';
 import { ModulesService } from 'src/api/services';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { ModuleVisibility } from 'src/api/models/module-visibility';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-first-step',
@@ -28,14 +29,34 @@ time: string;
 visibility: string;
 id: string;
 
+fullModel: ModuleResponse;
+
 modelR: ModuleCompactResponse;
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
       this.id = this.route.snapshot.paramMap.get('id');
+      console.log(this.id);
+      
+      if (this.id !== null){
+        this.fullModel = await this.modulesService.apiModulesIdGet$Json({id: +(this.id)}).toPromise();
+        console.log(this.fullModel);
+        this.module = this.fullModel;
+        this.class = this.fullModel.classLevel.toString();
+        this.visibility = this.fullModel.visibility.toString();
+        this.time = this.fullModel.laborIntensity.toString();
+      }
   }
 
-  onClickPut() {
+  async onClickPut() {
     this.router.navigate(['../../secondStep/', this.id], { relativeTo: this.route});
+    this.module.classLevel = +this.class;
+    this.module.laborIntensity = +this.time;
+    this.module.visibility = ModuleVisibility[this.visibility];
+    this.module.tags.push(this.tag);
+    await this.modulesService.apiModulesIdPut( {id: +(this.id),
+      body: this.module
+    }).toPromise();
+    this.router.navigate(['../secondStep/', this.modelR.id], { relativeTo: this.route});
   }
 
   async onClickCreate() {
